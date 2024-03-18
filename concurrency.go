bd4229bc31cf86631069dbe0e1325c4fb385690f
german <german.florezd@gmail.com>
2024-03-18 17:45:21 -0400

    G-002 concurrecy comparison on empty tasks

--- concurrency.go
+++ concurrency.go
@@ -8,26 +8,43 @@ import(
 var dbData = []string{"id1","id2","id3","id4","id5"}
 var wg = sync.WaitGroup{}
 var results = []string{}
-var m = sync.Mutex{}
+var m = sync.RWMutex{}
 
 func main(){
 	t0:= time.Now()
-	for i:=0; i<len(dbData); i++{
+	for i:=0; i<1000; i++{
 		wg.Add(1)
-		go dbCall(i)
+		go dbCall2(1)
 	}
 	wg.Wait()
 	fmt.Printf("\nTotal execution time: %v", time.Since(t0))
-	fmt.Printf("\nThe results are %v", results)
 }
+
+//since the function barely does anything, is not importatn how many times it's calles the time remains the same
+func dbCall2(i int){
+	var delay float32 = 2000
+	time.Sleep(time.Duration(delay)*time.Millisecond)
+	wg.Done()
+}
+
 
 func dbCall(i int){
 	var delay float32 = 2000
 	time.Sleep(time.Duration(delay)*time.Millisecond)
-	fmt.Println("The result from the database is:", dbData[i])
-	m.Lock()
-	results = append(results, dbData[i])
-	m.Unlock()
+	save(dbData[i])
+	log()
 	wg.Done()
+}
+
+func save(result string){
+	m.Lock()
+	results = append(results, result)
+	m.Unlock()
+}
+
+func log(){
+	m.RLock()
+	fmt.Printf("\nThe results are %v", results)
+	m.RUnlock()
 }
 
